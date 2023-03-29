c7064d5929a8fda03f6cb04e38357d258eec860e
ridwan1302210076 <rahmannurridwan@student.telkomuniversity.ac.id>
2023-03-29 17:19:50 +0700

    menambahkan dua class SayaTubevideo dan SayaclassUser

---
+++ ConsoleApp1/SayaTubeUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeUser> uploadedvideos;
+        private String Username;
+      
+
+        public SayaTubeUser(String Username)
+        {
+            this.Username = Username;
+            uploadedvideos = new List<SayaTubeUser>();
+            this.id = new Random().Next(10000, 99999);
+        }
+
+        public void GetTotalvideoPlaycount()
+        {
+            return uploadedvideos.Sum(x => x.playcount);
+        }
+        
+        public void AddVideo()
+        {
+
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/SayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        public int playCount;
+
+
+        public SayaTubeVideo(String title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int input)
+        {
+            this.playCount += input;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {this.id}\nTitle: {this.title}\nPlay Count: {this.playCount}\n");
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+        
+    }
+}
